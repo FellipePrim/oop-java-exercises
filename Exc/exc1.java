a3ea3cad145bae9bf64bc20122dccabb753d18b2
Fellipe Negherbon Prim <99996732+FellipePrim@users.noreply.github.com>
2024-08-05 13:53:31 -0300

    v1.0

---
+++ Exc/exc1.java
@@ -0,0 +1,11 @@
+public class exc1 {
+    public static void main(String[] args) throws Exception {
+        /* int 4 bytes -2,147,483,648 at´e 2,147,483,647 */
+        /* Escreva um programa em java que declare uma variável do tipo inteiro.
+        Atribua o maior valor possível para essa variável. Some mais 1 e apresente
+        o resultado em tela. Observe e analise o resultado. */
+
+        int number = 2147483647;
+        System.out.println(number+1);
+    }
+}
---
+++ Exc/exc2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class exc2 {
+    public static void main(String[] args) {
+        // Usando try-with-resources para garantir que o Scanner seja fechado automaticamente
+        try (Scanner scnr = new Scanner(System.in)) {
+            // Solicita ao usuário que insira um número inteiro
+            System.out.print("Choose an integer number: ");
+            int number = scnr.nextInt();
+            
+            // Verifica se o número é par ou ímpar
+            if (number % 2 == 0) {
+                System.out.println("The number " + number + " is even.");
+            } else {
+                System.out.println("The number " + number + " is odd.");
+            }
+        } // O Scanner é fechado automaticamente aqui
+    }
+}
+
