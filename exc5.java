c39b934544adfea2900c914b4fa640da024fefc1
Fellipe Negherbon Prim <99996732+FellipePrim@users.noreply.github.com>
2024-08-05 17:54:28 -0300

    v1.03

---
+++ exc5.java
@@ -0,0 +1,12 @@
+public class exc5 {
+    public static void main(String[] args) {
+        // Declara uma variável do tipo char
+        char txt;
+
+        // Atribui o valor inteiro 88 à variável txt
+        txt = (char) 88;
+
+        // Exibe o resultado em tela
+        System.out.println("The character corresponding to the integer value 88 is: " + txt);
+    }
+}
